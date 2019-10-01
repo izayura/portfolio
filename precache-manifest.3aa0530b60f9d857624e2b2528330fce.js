526c7214b499962d81bafb1a275956de715720b5
Gloria <izayura@gmail.com>
2019-10-01 17:40:40 -0500

    Updates

---
+++ precache-manifest.3aa0530b60f9d857624e2b2528330fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc6753b238e77387fa3214479b2617a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5070b22a9e6210bd1d9d",
+    "url": "/portfolio/static/css/main.b909b333.chunk.css"
+  },
+  {
+    "revision": "02d8a39cd8375e90fe24",
+    "url": "/portfolio/static/js/2.962f2ae7.chunk.js"
+  },
+  {
+    "revision": "5070b22a9e6210bd1d9d",
+    "url": "/portfolio/static/js/main.563526e7.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  },
+  {
+    "revision": "ac2c441d3f9279cdf43c2ac2a2f4c687",
+    "url": "/portfolio/static/media/vaporwaveCat.ac2c441d.jpg"
+  }
+]);
