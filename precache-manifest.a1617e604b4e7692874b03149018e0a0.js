b649d74063f8d455603b03f70313ff24bb21a41d
Gloria <izayura@gmail.com>
2019-11-12 13:59:12 -0600

    Updates

---
+++ precache-manifest.a1617e604b4e7692874b03149018e0a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7ecd812a9267c7cf0f62ccd32c182c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a5147726d5e763e55ec1",
+    "url": "/portfolio/static/css/main.53e13cd3.chunk.css"
+  },
+  {
+    "revision": "6171d724044cda89f11c",
+    "url": "/portfolio/static/js/2.d6c73cf9.chunk.js"
+  },
+  {
+    "revision": "a5147726d5e763e55ec1",
+    "url": "/portfolio/static/js/main.593aca9a.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  },
+  {
+    "revision": "b12d7dca411657fd1645b9db83e4573d",
+    "url": "/portfolio/static/media/Lazer84.b12d7dca.ttf"
+  },
+  {
+    "revision": "a71d4cbf475f3b77c5a8ff23e127215a",
+    "url": "/portfolio/static/media/galaxy.a71d4cbf.png"
+  },
+  {
+    "revision": "4039c7ffc2b79c2f0a4f9c9400e65fa0",
+    "url": "/portfolio/static/media/galaxy2.4039c7ff.png"
+  },
+  {
+    "revision": "d04b35a9ab4deab7347f582e4dd481a5",
+    "url": "/portfolio/static/media/name.d04b35a9.png"
+  },
+  {
+    "revision": "3d96ff51d5324d658177989738aafc4a",
+    "url": "/portfolio/static/media/select.3d96ff51.png"
+  },
+  {
+    "revision": "d4fc216db99ec93c8e1680c4c687eccf",
+    "url": "/portfolio/static/media/vaporwaveSmall.d4fc216d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9d2aba8218ef3c7a09232ac8ad3f7629.js"
+  "/portfolio/precache-manifest.a1617e604b4e7692874b03149018e0a0.js"
 );
 
 self.addEventListener('message', (event) => {
